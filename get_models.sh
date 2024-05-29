f19db15082dc64cab79477e949e2c4a22ecc7ae1
Paulo Linares <pe.lotoya93@gmail.com>
2024-05-29 18:47:59 +0800

    First acceptable version

--- get_models.sh
+++ get_models.sh
@@ -3,22 +3,23 @@
 # and place the models in the right folder.  
 
 # Ultralytics Human Pose Estimation model 
-wget https://github.com/ultralytics/assets/releases/download/v8.2.0/yolov8l-pose.pt
+wget https://github.com/Vegnics/SimpleDressingHMR/releases/download/v0.0.1/yolov8l-pose.pt
 mv yolov8l-pose.pt ./ultraytics/.
 
 #-----------------------------------------------------------------------------------
 
 # SMPL models (female, male, neutral | 10 PCs)
-# Male
-wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1S6Twwbk5QrfZM36bJKU0uNlg5W6OCJC6' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1S6Twwbk5QrfZM36bJKU0uNlg5W6OCJC6" -O basicmodel_f_lbs_10_207_0_v1.0.0.pkl && rm -rf /tmp/cookies.txt
-mv ./basicmodel_f_lbs_10_207_0_v1.0.0.pkl ./smpl/models/smpl/.
 
 # Female
-wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1FRrC8wzBVlOX77Rq_WdPrB4RoDaoZ-1L' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1FRrC8wzBVlOX77Rq_WdPrB4RoDaoZ-1L" -O basicmodel_m_lbs_10_207_0_v1.0.0.pkl && rm -rf /tmp/cookies.txt
+wget https://github.com/Vegnics/SimpleDressingHMR/releases/download/v0.0.1/basicmodel_f_lbs_10_207_0_v1.0.0.pkl
+mv ./basicmodel_f_lbs_10_207_0_v1.0.0.pkl ./smpl/models/smpl/.
+
+# Male
+wget https://github.com/Vegnics/SimpleDressingHMR/releases/download/v0.0.1/basicmodel_m_lbs_10_207_0_v1.0.0.pkl
 mv ./basicmodel_m_lbs_10_207_0_v1.0.0.pkl ./smpl/models/smpl/.
 
 # Neutral
-curl -H "Authorization: Bearer ya29.a0AXooCgvavov45Kqx0jmYslAABk2wbVTpAJajIfruzOwc70DdY3h5RkpErsY56-2O3qwZ-lSMJiNc1gYnIlUqUnW5EBxtjTpnT4byZ_uxOVApB1IjbkcFYBF9Xzc7igsz-pkwyUOlNGFtIHM4hCJpirgrUeID5rlRy_vCaCgYKASQSARISFQHGX2MiRSrfhyBRAw2zNZFerY9k8A0171" https://www.googleapis.com/drive/v3/files/19IUy6DVci_CSIK7msvcfpjOD4AT_dIHy?alt=media -o basicmodel_neutral_lbs_10_207_0_v1.1.0.pkl
+wget https://github.com/Vegnics/SimpleDressingHMR/releases/download/v0.0.1/basicmodel_neutral_lbs_10_207_0_v1.1.0.pkl
 mv ./basicmodel_neutral_lbs_10_207_0_v1.1.0.pkl ./smpl/models/smpl/.
 
 
@@ -27,8 +28,21 @@ mv ./basicmodel_neutral_lbs_10_207_0_v1.1.0.pkl ./smpl/models/smpl/.
 # HMR pre-trained models
 
 # Total capture - paired (most accurate)
-curl -H "Authorization: Bearer ya29.a0AXooCgvavov45Kqx0jmYslAABk2wbVTpAJajIfruzOwc70DdY3h5RkpErsY56-2O3qwZ-lSMJiNc1gYnIlUqUnW5EBxtjTpnT4byZ_uxOVApB1IjbkcFYBF9Xzc7igsz-pkwyUOlNGFtIHM4hCJpirgrUeID5rlRy_vCaCgYKASQSARISFQHGX2MiRSrfhyBRAw2zNZFerY9k8A0171" https://www.googleapis.com/drive/v3/files/10j4t0_F3rkcO0pBWsXdE_D402CiYG7-h?alt=media -o total_capture_model_paired.zip
+wget https://github.com/Vegnics/SimpleDressingHMR/releases/download/v0.0.1/total_capture_model_paired.zip
 mv ./total_capture_model_paired.zip ./SMPL_estimator/trained_models/.
 unzip ./SMPL_estimator/trained_models/total_capture_model_paired.zip -d ./SMPL_estimator/trained_models/.
 rm -f ./SMPL_estimator/trained_models/total_capture_model_paired.zip
+
+# Total capture - unpaired (works well when the person is not turning in the video)
+wget https://github.com/Vegnics/SimpleDressingHMR/releases/download/v0.0.1/total_capture_model.zip
+mv ./total_capture_model.zip ./SMPL_estimator/trained_models/.
+unzip ./SMPL_estimator/trained_models/total_capture_model.zip -d ./SMPL_estimator/trained_models/.
+rm -f ./SMPL_estimator/trained_models/total_capture_model.zip
+
+# Base (the shape parameters are not very accurate, unacceptable performance for the purpose of this project)
+wget https://github.com/Vegnics/SimpleDressingHMR/releases/download/v0.0.1/base_model.zip
+mv ./base_model.zip ./SMPL_estimator/trained_models/.
+unzip ./SMPL_estimator/trained_models/base_model.zip -d ./SMPL_estimator/trained_models/.
+rm -f ./SMPL_estimator/trained_models/base_model.zip
+
 
