cbb6b9329bc4915e6488d4ba6afb38cf2f66a411
Paulo Linares <pe.lotoya93@gmail.com>
2024-06-08 08:08:27 +0800

    First release

--- get_models.sh
+++ get_models.sh
@@ -6,7 +6,7 @@
 echo "Downloading Ultralytics pre-trained model ..."
 mkdir -p ./ultralytics 
 wget https://github.com/Vegnics/SimpleDressingHMR/releases/download/v0.0.1/yolov8l-pose.pt
-mv yolov8l-pose.pt ./ultraytics/.
+mv -f yolov8l-pose.pt ./ultraytics/.
 
 #-----------------------------------------------------------------------------------
 
@@ -17,15 +17,15 @@ mkdir -p ./smpl/models/smpl
 echo "Downloading SMPL models ..."
 # Female
 wget https://github.com/Vegnics/SimpleDressingHMR/releases/download/v0.0.1/basicmodel_f_lbs_10_207_0_v1.0.0.pkl
-mv ./basicmodel_f_lbs_10_207_0_v1.0.0.pkl ./smpl/models/smpl/.
+mv -f ./basicmodel_f_lbs_10_207_0_v1.0.0.pkl ./smpl/models/smpl/.
 
 # Male
 wget https://github.com/Vegnics/SimpleDressingHMR/releases/download/v0.0.1/basicmodel_m_lbs_10_207_0_v1.0.0.pkl
-mv ./basicmodel_m_lbs_10_207_0_v1.0.0.pkl ./smpl/models/smpl/.
+mv -f ./basicmodel_m_lbs_10_207_0_v1.0.0.pkl ./smpl/models/smpl/.
 
 # Neutral
 wget https://github.com/Vegnics/SimpleDressingHMR/releases/download/v0.0.1/basicmodel_neutral_lbs_10_207_0_v1.1.0.pkl
-mv ./basicmodel_neutral_lbs_10_207_0_v1.1.0.pkl ./smpl/models/smpl/.
+mv -f ./basicmodel_neutral_lbs_10_207_0_v1.1.0.pkl ./smpl/models/smpl/.
 
 
 #-----------------------------------------------------------------------------------
@@ -37,19 +37,19 @@ echo "Downloading HMR pre-trained models ..."
 # Total capture - paired (most accurate)
 wget https://github.com/Vegnics/SimpleDressingHMR/releases/download/v0.0.1/total_capture_model_paired.zip
 mv ./total_capture_model_paired.zip ./SMPL_estimator/trained_models/.
-unzip ./SMPL_estimator/trained_models/total_capture_model_paired.zip -d ./SMPL_estimator/trained_models/.
+unzip -o ./SMPL_estimator/trained_models/total_capture_model_paired.zip -d ./SMPL_estimator/trained_models/.
 rm -f ./SMPL_estimator/trained_models/total_capture_model_paired.zip
 
 # Total capture - unpaired (works well when the person is not turning in the video)
 wget https://github.com/Vegnics/SimpleDressingHMR/releases/download/v0.0.1/total_capture_model.zip
 mv ./total_capture_model.zip ./SMPL_estimator/trained_models/.
-unzip ./SMPL_estimator/trained_models/total_capture_model.zip -d ./SMPL_estimator/trained_models/.
+unzip -o ./SMPL_estimator/trained_models/total_capture_model.zip -d ./SMPL_estimator/trained_models/.
 rm -f ./SMPL_estimator/trained_models/total_capture_model.zip
 
 # Base (the shape parameters are not very accurate, unacceptable performance for the purpose of this project)
 wget https://github.com/Vegnics/SimpleDressingHMR/releases/download/v0.0.1/base_model.zip
 mv ./base_model.zip ./SMPL_estimator/trained_models/.
-unzip ./SMPL_estimator/trained_models/base_model.zip -d ./SMPL_estimator/trained_models/.
+unzip -o ./SMPL_estimator/trained_models/base_model.zip -d ./SMPL_estimator/trained_models/.
 rm -f ./SMPL_estimator/trained_models/base_model.zip
 
 mkdir -p ./videos
